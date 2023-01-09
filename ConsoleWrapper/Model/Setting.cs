73e0aaaabb9f4cc91febfa8e2d5aa09b0aa6d77b
rinjugatla <rin.jugatla@gmail.com>
2023-01-09 14:05:34 +0900

    マクロ定義設定ファイルを追加

---
+++ ConsoleWrapper/Model/Setting.cs
@@ -0,0 +1,104 @@
+﻿using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ConsoleWrapper.Model
+{
+    public partial class Setting
+    {
+        [JsonProperty("app")]
+        public App App { get; set; }
+
+        [JsonProperty("basic_commands")]
+        public List<BasicCommand> BasicCommands { get; set; }
+
+        [JsonProperty("macro_commands")]
+        public List<MacroCommand> MacroCommands { get; set; }
+    }
+
+    public partial class App
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public partial class BasicCommand
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("commands")]
+        public List<BasicCommandCommand> Commands { get; set; }
+
+        [JsonProperty("subcommands", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Subcommand> Subcommands { get; set; }
+    }
+
+    public partial class BasicCommandCommand
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("command")]
+        public string Command { get; set; }
+
+        [JsonProperty("pattern")]
+        public string Pattern { get; set; }
+    }
+
+    public partial class Subcommand
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("pattern", NullValueHandling = NullValueHandling.Ignore)]
+        public string Pattern { get; set; }
+    }
+
+    public partial class MacroCommand
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("commands")]
+        public List<MacroCommandCommand> Commands { get; set; }
+    }
+
+    public partial class MacroCommandCommand
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("command")]
+        public string Command { get; set; }
+    }
+
+    public partial class Setting
+    {
+        public static List<Setting> FromJson(string json) => JsonConvert.DeserializeObject<List<Setting>>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this List<Setting> self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,97 @@
 # ConsoleWrapper
+
+## CommandSetting.json
+
+コマンド定義サンプル
+
+```json
+[
+    {
+        "app": {
+            "name": "bedrock_server.exe"
+        },
+        "basic_commands": [
+            {
+                "name": "show help",
+                "description": "",
+                "commands": [
+                    {
+                        "type": "console",
+                        "command": "? <int>",
+                        "pattern": "^\\? (?<page>1-3)$"
+                    }
+                ]
+            },
+            {
+                "name": "show gamerule",
+                "description": "",
+                "commands": [
+                    {
+                        "type": "console",
+                        "command": "gamerule",
+                        "pattern": ""
+                    }
+                ],
+                "subcommands": [
+                    {
+                        "name": ""
+                    }
+                ]
+            },
+            {
+                "name": "set gamerule",
+                "description": "",
+                "commands": [
+                    {
+                        "type": "gamerule <text>",
+                        "command": "",
+                        "pattern": "^gamerule (?<name>[A-Za-z]+)$"
+                    }
+                ],
+                "subcommands": [
+                    {
+                        "name": "name",
+                        "pattern": ""
+                    }
+                ]
+            },
+            {
+                "name": "template",
+                "description": "",
+                "commands": [
+                    {
+                        "type": "",
+                        "command": "",
+                        "pattern": ""
+                    }
+                ],
+                "subcommands": [
+                    {
+                        "name": ""
+                    }
+                ]
+            }
+        ],
+        "macro_commands": [
+            {
+                "name": "restart",
+                "description": "",
+                "commands": [
+                    {
+                        "type": "console",
+                        "command": "say Restart the server after 3 minutes."
+                    },
+                    {
+                        "type": "system",
+                        "command": "wait 3 min"
+                    },
+                    {
+                        "type": "console",
+                        "command": "stop"
+                    }
+                ]
+            }
+        ]
+    }
+]
+```
